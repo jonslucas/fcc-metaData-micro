f560f6cbe77053eb8c9d5103af31c2f3d6659e6b
Jon Lucas <jon.s.lucas@gmail.com>
2016-02-23 17:35:13 -0500

    Finished implementing multer file analysis.

--- app/index.js
+++ app/index.js
@@ -2,9 +2,11 @@
     'use strict';
     const express = require('express'),
            routes = require('./routes'),
+           multer = require('multer'),
               app = express();
 
     routes(app);
+    //app.use(multer({dest: './uploads/'}));
 
     app.use('/public', express.static(process.cwd() + '/public'));
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,21 @@
 (()=>{
     'use strict';
+    const multer = require('multer');
 
     module.exports = (app)=>{
       app.route('/')
           .get((req, res)=>{
               res.sendFile(process.cwd() + '/public/html/index.html');
           })
+          .post(multer().single('file'), (req, res)=>{
+              const fl = req.file,
+                  resp = {
+                      fileName: fl.originalname,
+                      fileSize: fl.size + ' bytes'
+                  };
+
+              res.send(resp);
+
+          });
     };
 })();
